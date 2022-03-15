10169fc002abbb80e99c0c3f79c25f59c1fecbda
Mark Hartman <whispergreencorvus@gmail.com>
2022-03-15 12:35:27 +0100

    removed usless rows

--- src/main/java/com/hartmanmark/inteherdivision/exception/DividendIsLessThanDivisorException.java
+++ src/main/java/com/hartmanmark/inteherdivision/exception/DividendIsLessThanDivisorException.java
@@ -2,8 +2,6 @@ package com.hartmanmark.inteherdivision.exception;
 
 public class DividendIsLessThanDivisorException extends Exception {
 
-    private static final long serialVersionUID = 1L;
-
     public DividendIsLessThanDivisorException(String message) {
         super(message);
     }
--- src/main/java/com/hartmanmark/inteherdivision/exception/DivisorIsZeroException.java
+++ src/main/java/com/hartmanmark/inteherdivision/exception/DivisorIsZeroException.java
@@ -1,8 +1,6 @@
 package com.hartmanmark.inteherdivision.exception;
 
-public class DivisorIsZeroException extends Exception{
-
-    private static final long serialVersionUID = 1L;
+public class DivisorIsZeroException extends Exception {
 
     public DivisorIsZeroException(String message) {
         super(message);
